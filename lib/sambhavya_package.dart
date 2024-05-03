7c1dd037a52def19c469b312a919415f46cf32f0
AsimBhadra <ashimbhadra.work@gmail.com>
2024-05-03 23:30:29 +0545

    Exported counsellors

--- lib/sambhavya_package.dart
+++ lib/sambhavya_package.dart
@@ -1,3 +1,4 @@
 library sambhavya_package;
 
 export 'src/modules/appointments/appointment_export.dart';
+export 'src/modules/counsellors/counsellors_export.dart';
---
+++ lib/src/modules/counsellors/counsellors_export.dart
@@ -0,0 +1,5 @@
+// Entities export
+export 'domain/entities/counsellor/counsellor.dart';
+
+// Datasources export
+export 'infra/datasources/counsellors_provider.dart';
