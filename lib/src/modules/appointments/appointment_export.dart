0659f6521c4658c1a8936669b1be742743f310e3
AsimBhadra <ashimbhadra.work@gmail.com>
2024-05-11 11:47:53 +0545

    Export

--- lib/src/modules/appointments/appointment_export.dart
+++ lib/src/modules/appointments/appointment_export.dart
@@ -7,6 +7,7 @@ export 'domain/entities/appointmentType/appointment_type.dart';
 export 'infra/datasources/appointmentLocation/appointment_location_datasource.dart';
 export 'infra/datasources/myAppointments/my_appointments_datasource.dart';
 export 'infra/datasources/myLatestAppointment/my_latest_appointment_datasource.dart';
+export 'infra/datasources/counsellorAppointment/counsellor_appointment_datasource.dart';
 
 // Repositories export
 export 'infra/repositories/appointmentDb/appointment_db_repository.dart';
