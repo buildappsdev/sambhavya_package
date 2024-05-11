f0ae3a5083cca15a3951cff5d6eb455b4915916d
AsimBhadra <ashimbhadra.work@gmail.com>
2024-05-11 16:34:36 +0545

    Removed

--- lib/src/modules/counsellors/counsellors_export.dart
+++ lib/src/modules/counsellors/counsellors_export.dart
@@ -3,4 +3,3 @@ export 'domain/entities/counsellor/counsellor.dart';
 
 // Datasources export
 export 'infra/datasources/counsellors_provider.dart';
-export 'infra/datasources/counsellor_availability_datasource.dart';
--- lib/src/modules/counsellors/infra/repositories/counsellor_db_repository.dart
+++ lib/src/modules/counsellors/infra/repositories/counsellor_db_repository.dart
@@ -1,6 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:sambhavya_package/src/modules/appointments/infra/datasources/counsellorAppointment/counsellor_appointment_datasource.dart';
 import 'package:sambhavya_package/src/modules/counsellors/domain/entities/counsellor/counsellor.dart';
 import 'package:sambhavya_package/src/modules/firebase/firestore/infra/repositories/firestore_repository.dart';
 
@@ -24,32 +23,5 @@ class _CounsellorDatabaseController {
     return counsellorsSnapshot.docs
         .map((doc) => Counsellor.fromJson(doc.data()))
         .toList();
-  }
-
-  Future<Map<int, List<int>>> getCounsellorAvailability(
-      Counsellor counsellor) async {
-    final availableDays = counsellor.workingDays;
-    final availableHours = counsellor.workingHours;
-
-    final availability = <int, List<int>>{};
-
-    for (var day in availableDays) {
-      availability[day] = availableHours.toList();
-    }
-
-    _ref
-        .read(counsellorsAppointmentsProvider(counsellor.id))
-        .whenData((appointments) {
-      for (var appointment in appointments) {
-        final appointmentDay = appointment.dateTime.weekday;
-        final appointmentHour = appointment.dateTime.hour;
-
-        availability[appointmentDay]!.remove(appointmentHour);
-      }
-    });
-
-    availability.removeWhere((key, value) => value.isEmpty);
-
-    return availability;
   }
 }
