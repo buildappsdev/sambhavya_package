10d998f6aa901868ef85404389e203fe0d32ddb3
AsimBhadra <ashimbhadra.work@gmail.com>
2024-05-11 16:29:10 +0545

    Updates

--- lib/src/modules/counsellors/infra/datasources/counsellor_availability_datasource.dart
+++ lib/src/modules/counsellors/infra/datasources/counsellor_availability_datasource.dart
@@ -2,9 +2,8 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:sambhavya_package/sambhavya_package.dart';
 import 'package:sambhavya_package/src/modules/counsellors/infra/repositories/counsellor_db_repository.dart';
 
-final counsellorAvailabilityProvider =
-    FutureProvider.family<Map<int, List<int>>, Counsellor>(
-        (ref, counsellor) async {
+final counsellorAvailabilityProvider = FutureProvider.family
+    .autoDispose<Map<int, List<int>>, Counsellor>((ref, counsellor) async {
   return await ref
       .read(counsellorDatabaseProvider)
       .getCounsellorAvailability(counsellor);
