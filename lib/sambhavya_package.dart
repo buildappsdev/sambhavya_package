033841d01a29c2eff579af51812eb9ba6787bbd7
AsimBhadra <ashimbhadra.work@gmail.com>
2024-05-31 09:55:01 +0545

    Fix export

--- lib/sambhavya_package.dart
+++ lib/sambhavya_package.dart
@@ -2,5 +2,6 @@ library sambhavya_package;
 
 export 'src/modules/appointments/appointment_export.dart';
 export 'src/modules/counsellors/counsellors_export.dart';
+export 'src/modules/notifications/notification_export.dart';
 
 export 'src/utils/utils_export.dart';
